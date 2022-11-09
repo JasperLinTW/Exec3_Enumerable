0649cf5739287593ae1254d2f99302bb8da1265b
Jasper <guitar1859@gmail.com>
2022-11-09 09:46:03 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //利用Enumerable.Range() 建立一個序列, 值介於 [1, 10]
+            //請利用迴圈列出每一個項目值,確認無誤
+
+            IEnumerable<int> items = Enumerable.Range(1, 10);
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //用Enumerable.Range() 建立一個序列, 值介於[10, 15]
+            //請利用迴圈列出每一個項目值,確認無誤
+            IEnumerable<int> ints = Enumerable.Range(10, 6);
+            foreach (var item in ints)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
